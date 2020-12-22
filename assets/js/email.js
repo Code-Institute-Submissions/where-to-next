3cac03f63c274381093cd62e9bf110aa6e7a4d29
hollyford <holly_ford@hotmail.com>
2020-12-22 18:09:26 +0000

    fixed email API so email now sends and also recognises the tick boxes as positive or negative. Also started on styling/CSS but not finished

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,18 +1,22 @@
-        function sendMail(contactForm) {
-    emailjs.send("service_jysqc4u", "template_h6e5rey", {
+    function sendMail(contactForm) {
+        console.log("credit", contactForm.credit.checked);
+        emailjs.send("service_jysqc4u", "template_h6e5rey", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "feedback": contactForm.feedback.value,
-        "credit": contactForm.credit.value,
-        "more_info": contactForm.more_info.value,
+        "credit": contactForm.credit.checked,
+        "more_info": contactForm.more_info.checked,
     })
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            alert("Thanks for your feedback");
         },
             function(error) {
                 console.log("FAILED", error);
+                alert("Something went wrong, can you send that again?");
             }
         );
     return false;
 };
+
