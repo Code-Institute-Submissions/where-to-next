a4bdc1c289cd659dde6d4bda3fd531a56a55f850
hollyford <holly_ford@hotmail.com>
2020-12-26 17:25:19 +0000

    adding changes during mentor call

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,22 +1,33 @@
+function validateRequiredInput(input){
+    if (input.value.length < 3){
+        return false;
+    }
+    return true;
+}
     function sendMail(contactForm) {
+        if (validateRequiredInput(contactForm.name) && validateRequiredInput(contactForm.emailaddress) && validateRequiredInput(contactForm.feedback))
+      {
         console.log("credit", contactForm.credit.checked);
         emailjs.send("service_jysqc4u", "template_h6e5rey", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "feedback": contactForm.feedback.value,
-        "credit": contactForm.credit.checked,
-        "more_info": contactForm.more_info.checked,
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-            alert("Thanks for your feedback");
-        },
-            function(error) {
-                console.log("FAILED", error);
-                alert("Something went wrong, can you send that again?");
-            }
-        );
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "feedback": contactForm.feedback.value,
+            "credit": contactForm.credit.checked,
+            "more_info": contactForm.more_info.checked,
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+                alert("Thanks for your feedback");
+            },
+                function(error) {
+                    console.log("FAILED", error);
+                    alert("Something went wrong, can you send that again?");
+                }
+            );
+      }
+      else{
+        alert("Please fill required fields");
+      }
     return false;
 };
-
